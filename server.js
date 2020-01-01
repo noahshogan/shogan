1cdea362ea6aef510493530645e00c9ba91d0a95
noahsh <noahshogan@gmail.com>
2020-01-01 22:42:45 +0200

    test

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const winston = require('winston');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+        new winston.transports.File({filename: 'combined.log'})
+    ]
+});
+
+let counter = 0;
+app.get('/', function(req, res){
+    res.send('Hi, you are visitore number ' + ++counter);
+    logger.info("{" + counter + "}");
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
