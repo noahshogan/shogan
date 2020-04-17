c42035a32a9b7243360bc729268f404382e56d67
noahsh <noahshogan@gmail.com>
2020-04-17 23:52:55 +0300

    working pull from db username

--- server.js
+++ server.js
@@ -15,10 +15,36 @@ const logger = winston.createLogger({
 });
 
 let counter = 0;
-app.get('/', function(req, res){
-    res.send('Hi, you are visitore number ' + ++counter);
+
+app.get('/', function (req, res) {
+    mysql().then(function (ans) {
+        res.send('Hi,' + ans + ' you are visitore number ' + counter);
+    });
     logger.info("{" + counter + "}");
 });
 
+function mysql() {
+    return new Promise(function (resolve, reject) {
+        let mysql = require('mysql');
+        let con = mysql.createConnection({
+            host: "us-cdbr-iron-east-05.cleardb.net",
+            user: "b18b59a176d884",
+            password: "b154c21c",
+            database: "heroku_cf1ed04491a650c"
+        });
+
+        con.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+            let sql = "select * from shogan;";
+            con.query(sql, function (err, result) {
+                // if (err) throw err;
+                console.log("Result: " + result[0].name);
+                resolve(result[0].name);
+            });
+            con.end()
+        });
+    })
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
